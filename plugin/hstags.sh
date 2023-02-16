c1a061fe2efcda6e4f2967c3a2824116506e6d9e
user7723 <jmp.loop@kittens.org>
2023-02-16 10:06:26 +0300

    refactored tags generation logic

--- plugin/hstags.sh
+++ plugin/hstags.sh
@@ -3,9 +3,32 @@
 # produces
 #   .tags - ctags file for the current project
 #   .hscope.db - cscope file for the current project
+# Err 1 - no ctags gen tool
+# Err 2 - no cscope gen tool
+# Err 3 - no haskell files
+# Err 4 - command execution error
+# Err 5 - output file generation failure
 
-tags_file=".tags"
-scope_file=".hscope.db"
+ctags_file=".tags"
+cscope_file=".hscope.db"
+
+ctags_gen='ghc-tags'
+ctags_flags='-c -f '$ctags_file
+
+cscope_gen='hscope'
+cscope_flags='-b -f '$cscope_file
+
+# Err 1 - no ctags gen tool
+if [ ! $(which "$ctags_gen") ] ; then
+  printf "Error: $ctags_gen was not found"
+  exit 1
+fi
+
+# Err 2 - no cscope gen tool
+if [ ! $(which "$cscope_gen") ] ; then
+  printf "Error: $cscope_gen was not found"
+  exit 2
+fi
 
 # list of .hs files in the current repo
 srcs=$(git ls-files -- '*.hs')
@@ -15,21 +38,43 @@ srcs=$(git ls-files -- '*.hs')
   # but exclude all hidden directories
   srcs=$(find . \( ! -regex '.*/\..*' \) -type f -name "*.hs")
 
+# Err 3 - no haskell files
+if [ -z "$srcs" ] ; then
+  printf "Error: No haskell files under $(pwd) directory"
+  exit 3
+fi
+
 # make ctags or cscope file for all .hs files of the current project
 function generate () {
-  toggle="$1"
-  out="$2"
-  gencmd='ghc-tags -c'
-  [ "$toggle" == "ctags" ] || gencmd='hscope -b'
+  out="$1"
 
-  cmd='echo "$srcs" | xargs '$gencmd' -f '$out
+  if [ "$out" == "$cscope_file" ] ; then
+    gencmd="$cscope_gen $cscope_flags"
+  else
+    gencmd="$ctags_gen $ctags_flags"
+  fi
 
-  action="Updating"
-  [ -f "$out" ] || action="Generating"
+  cmd='echo "$srcs" | xargs '$gencmd
 
-  echo "${action} ${toggle} file (${out}) for project source code..."
-  eval "$cmd" && echo "done"
+  err="$(eval $cmd 2>&1>/dev/null)"
+  if [ -n "$err" ] ; then
+    printf "Error: $err" | perl -pe 's/\n//g'
+    exit 4
+  fi
+
+  # Err 4 - file generation failure
+  if [ ! -f "$out" ] ; then
+    printf "Error: failed to generate \"$out\" file"
+    exit 5
+  fi
 }
 
-generate 'ctags' "$tags_file"
-generate 'cscope' "$scope_file"
+t_action="generated"
+[ -f "$ctags_file" ] && t_action="updated"
+
+s_action="generated"
+[ -f "$cscope_file" ] && s_action="updated"
+
+generate "$ctags_file"
+generate "$cscope_file"
+printf "Succes: $t_action $ctags_file file and $s_action $cscope_file file"
